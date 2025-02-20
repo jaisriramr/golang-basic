2cf0d8772788f00e3a30070c933172ed5d6f20a2
jaisriram <jaisriram.webdeveloper@gmail.com>
2025-02-20 11:34:34 +0530

    learn

--- for-loop-array.go
+++ for-loop-array.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 
-	numbers := []int{10, 20, 30}
+	nums := []int{10, 20, 30}
 
-	for index, value := range numbers {
-		fmt.Println("Index:", index, "Value:", value)
+	for index, value := range nums {
+		fmt.Println(index, value)
 	}
 
 }
